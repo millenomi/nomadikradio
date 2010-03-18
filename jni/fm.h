b75970e538ec82bd6d9e58a185d00dfe8c5f9955
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2010-03-18 14:18:17 +0100

    Sketched out the API in fm.h.

--- jni/fm.h
+++ jni/fm.h
@@ -7,3 +7,21 @@
  *
  */
 
+#include <stdbool.h>
+
+typedef void FMRadio;
+
+extern FMRadio* FMRadioOpen(const char* deviceFile);
+extern void FMRadioClose(FMRadio* f); // deallocs it too
+
+extern void FMSetTurnedOn(FMRadio* r, bool on);
+
+extern void FMSetFrequency(FMRadio* r, int hz);
+
+enum {
+	kFMVolumeMute = 0,
+	kFMVolumeMaximum = 65536,
+};
+extern int FMGetVolume(FMRadio* r);
+extern void FMSetVolume(FMRadio* r, int volume);
+
